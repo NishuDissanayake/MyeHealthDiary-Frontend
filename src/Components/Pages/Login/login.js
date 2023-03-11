7b78f3ff2bd6df0a5c8bf22f807b85a9b91807c5
NishuDissanayake <nishudissanayake5252@gmail.com>
2023-03-11 15:45:17 +0530

    Login completed

--- src/Components/Pages/Login/login.js
+++ src/Components/Pages/Login/login.js
@@ -1,10 +1,46 @@
 import React from 'react';
 import './login.css';
+import loginImage from './../../../Assets/Login Image.jpg';
+import {
+  MDBInput,
+  MDBCol,
+  MDBRow,
+  MDBCheckbox,
+  MDBBtn,
+  MDBIcon
+} from 'mdb-react-ui-kit';
 
-function login() {
+export default function login() {
   return (
-    <div>login</div>
-  )
+    <form className='loginForm'>
+      <div className='formContent center'>
+        <MDBCol>
+        <MDBRow className='mb-4 mainColLogin'>
+          <span className='loginText'>Login</span>
+          <MDBRow className='inputLogin'>
+            <label className='loginLText'>Email Address</label>
+            <MDBInput className='inputField' id='email' type='text' />
+          </MDBRow>
+          <MDBRow className='inputLogin'>
+            <label className='loginLText'>Password</label>
+            <MDBInput className='inputField' id='pwd' type='password' />
+          </MDBRow>
+          <MDBRow>
+            <MDBBtn type='submit' className='mb-4 btnLogin' block>Login</MDBBtn>
+          </MDBRow>
+          <MDBRow>
+            <div className='text-center'>
+              <p>
+                Don't have an account? <span className='signupBText'><a href='#!'>Sign Up</a></span>
+              </p>
+            </div>
+          </MDBRow>
+        </MDBRow>
+        </MDBCol>
+        <MDBCol>
+          <img src={loginImage} className='loginImageStyle' />
+        </MDBCol>
+      </div>
+    </form >
+  );
 }
-
-export default login
