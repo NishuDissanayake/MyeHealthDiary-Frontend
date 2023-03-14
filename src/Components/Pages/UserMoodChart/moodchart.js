0624eca82770fb50a0c684c22d36ebf569c3bc64
NishuDissanayake <nishudissanayake5252@gmail.com>
2023-03-14 21:02:22 +0530

    Added mood chart sample

--- src/Components/Pages/UserMoodChart/moodchart.js
+++ src/Components/Pages/UserMoodChart/moodchart.js
@@ -2,6 +2,30 @@ import { MDBContainer, MDBRow, MDBCol } from 'mdb-react-ui-kit';
 import React from 'react';
 import './moodchart.css';
 import moodIco from './../../../Assets/mood.png';
+import Chart from 'react-google-charts';
+
+const LineData = [
+  ['x', 'dogs', 'cats'],
+  [0, 0, 0],
+  [1, 10, 5],
+  [2, 23, 15],
+  [3, 17, 9],
+  [4, 18, 10],
+  [5, 9, 5],
+  [6, 11, 3],
+  [7, 27, 19],
+]
+const LineChartOptions = {
+  hAxis: {
+    title: 'Time',
+  },
+  vAxis: {
+    title: 'Popularity',
+  },
+  series: {
+    1: { curveType: 'function' },
+  },
+}
 
 function moodchart() {
   return (
@@ -14,7 +38,18 @@ function moodchart() {
       </MDBRow>
 
       <MDBContainer>
-
+        <div className="container mt-5">
+          <h2>Mood Chart Line Graph Example</h2>
+          <Chart
+            width={'700px'}
+            height={'410px'}
+            chartType="LineChart"
+            loader={<div>Loading Chart</div>}
+            data={LineData}
+            options={LineChartOptions}
+            rootProps={{ 'data-testid': '2' }}
+          />
+        </div>
       </MDBContainer>
 
     </MDBContainer>
